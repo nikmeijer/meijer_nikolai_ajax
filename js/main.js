2562874a6f259b9f9a3209006599e8cbee2f7551
nikmeijer <n_meijer@fanshaweonline.ca>
2023-11-21 14:11:56 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,151 @@
+(() => {
+
+  //variables
+  const model = document.querySelector("#model");
+  const hotspots = document.querySelectorAll(".Hotspot");
+  const materialTemplate = document.querySelector("#materials-template");
+  const materialList = document.querySelector("#materials-list");
+  const matsInfo = document.querySelector("#mats-info");
+
+  let spinner = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin: auto; background: rgb(241, 242, 243); display: block; shape-rendering: auto;" width="200px" height="200px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+<g transform="rotate(0 50 50)">
+  <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#5f2a62">
+    <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.9166666666666666s" repeatCount="indefinite"></animate>
+  </rect>
+</g><g transform="rotate(30 50 50)">
+  <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#a976c3">
+    <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.8333333333333334s" repeatCount="indefinite"></animate>
+  </rect>
+</g><g transform="rotate(60 50 50)">
+  <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#a0de59">
+    <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.75s" repeatCount="indefinite"></animate>
+  </rect>
+</g><g transform="rotate(90 50 50)">
+  <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#466b5a">
+    <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.6666666666666666s" repeatCount="indefinite"></animate>
+  </rect>
+</g><g transform="rotate(120 50 50)">
+  <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#f5c024">
+    <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.5833333333333334s" repeatCount="indefinite"></animate>
+  </rect>
+</g><g transform="rotate(150 50 50)">
+  <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#5f2a62">
+    <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.5s" repeatCount="indefinite"></animate>
+  </rect>
+</g><g transform="rotate(180 50 50)">
+  <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#a976c3">
+    <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.4166666666666667s" repeatCount="indefinite"></animate>
+  </rect>
+</g><g transform="rotate(210 50 50)">
+  <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#a0de59">
+    <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.3333333333333333s" repeatCount="indefinite"></animate>
+  </rect>
+</g><g transform="rotate(240 50 50)">
+  <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#466b5a">
+    <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.25s" repeatCount="indefinite"></animate>
+  </rect>
+</g><g transform="rotate(270 50 50)">
+  <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#f5c024">
+    <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.16666666666666666s" repeatCount="indefinite"></animate>
+  </rect>
+</g><g transform="rotate(300 50 50)">
+  <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#5f2a62">
+    <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.08333333333333333s" repeatCount="indefinite"></animate>
+  </rect>
+</g><g transform="rotate(330 50 50)">
+  <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#a976c3">
+    <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="0s" repeatCount="indefinite"></animate>
+  </rect>
+</g>`
+
+  //This information needs to be removed then pulled with an AJAX Call using the Fetch API
+  //this is the api url https://swiftpixel.com/earbud/api/infoboxes"
+
+
+
+  //This information needs to be removed then pulled with an AJAX Call using the Fetch API
+  //this is the api url https://swiftpixel.com/earbud/api/materials"
+
+
+
+  //functions
+  function modelLoaded() {
+    hotspots.forEach(hotspot => {
+      hotspot.style.display = "block";
+    });
+  }
+
+  function loadInfoBoxes() {
+
+    fetch("https://swiftpixel.com/earbud/api/infoboxes")
+      .then(response => response.json())
+      .then(infoloop => {
+
+        infoloop.forEach((loop, index) => {
+          let selected = document.querySelector(`#hotspot-${index + 1}`);
+
+          const titleElement = document.createElement('h2');
+          titleElement.textContent = loop.heading;
+
+          const textElement = document.createElement('p');
+          textElement.textContent = loop.description;
+
+          selected.appendChild(titleElement);
+          selected.appendChild(textElement);
+        });
+      })
+      .catch(oops => console.error(oops));
+  }
+  loadInfoBoxes();
+
+
+  function loadMaterialInfo() {
+    matsInfo.innerHTML = spinner;
+
+    fetch("https://swiftpixel.com/earbud/api/materials")
+      .then(response => response.json())
+      .then(matloop => {
+
+        matloop.forEach(mats => {
+          // Clone the template content
+          const clone = materialTemplate.content.cloneNode(true);
+          //const clone = document.importNode(materialTemplate.content, true);
+
+          // Populate the cloned template with data
+          const materialHeading = clone.querySelector(".materials-heading");
+          materialHeading.textContent = mats.heading;
+
+          const materialDescription = clone.querySelector(".materials-description");
+          materialDescription.textContent = mats.description;
+
+          // Append the populated template to the list
+          materialList.appendChild(clone);
+
+        });
+        matsInfo.innerHTML = ""
+        matsInfo.appendChild(materialList)
+      })
+
+      .catch(oops => console.error(oops));
+  }
+  loadMaterialInfo();
+
+  function showInfo() {
+    let selected = document.querySelector(`#${this.slot}`);
+    gsap.to(selected, 1, { autoAlpha: 1 });
+  }
+
+  function hideInfo() {
+    let selected = document.querySelector(`#${this.slot}`);
+    gsap.to(selected, 1, { autoAlpha: 0 });
+  }
+
+  //Event listeners
+  model.addEventListener("load", modelLoaded);
+
+  hotspots.forEach(function (hotspot) {
+    hotspot.addEventListener("mouseenter", showInfo);
+    hotspot.addEventListener("mouseleave", hideInfo);
+  });
+
+})();
